9995de52eb5513dcc731b53b0ad26fd7c9705f71
Student <example@example.com>
2019-03-22 08:22:55 -0700

    initial commit for Friday project.

--- src/business-logic.js
+++ src/business-logic.js
@@ -2,7 +2,7 @@ export class apiCall {
   runGithub() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let api_key = "9fbdddd24530392616432c7c9bf1c241";
+      let api_key = "42a9758028d71c3bfdd8c7f1898b2e21";
       let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=41.8781,-87.6298,100&skip=2&limit=10&user_key=' + api_key;
       request.onload = function() {
         if (this.status === 200) {

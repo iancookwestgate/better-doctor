775bc6d8c6a0dbf18ea1afa73650096a83594706
Student <example@example.com>
2019-03-22 09:18:17 -0700

    get the .env functionality working.

--- src/business-logic.js
+++ src/business-logic.js
@@ -2,8 +2,8 @@ export class apiCall {
   runGithub() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      // let api_key = "42a9758028d71c3bfdd8c7f1898b2e21";
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=45.5122,-122.6587,100&skip=2&limit=10&user_key=' + process.env.betterDoctor;
+      let api_key = process.env.betterDoctor;
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=45.5122,-122.6587,100&skip=2&limit=10&user_key=' + api_key;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);

0e95b6bd5007f007149a459041672d66b8a1144b
Student <example@example.com>
2019-03-22 09:56:13 -0700

    add fail trigger if input field is left empty.

--- src/main.js
+++ src/main.js
@@ -5,14 +5,50 @@ import './sass/styles.scss';
 import { apiCall } from "./../src/business-logic.js";
 
 $(document).ready(function() {
-  $(".callApi").click(function() {
+  $(".lookCondition").click(function() {
+    $(".output").html("");
+
+    if($("#condition").val() == ""){
+      alert("Please input your condition.");
+    } else {
+      let freshApi = new apiCall();
+      let promise = freshApi.runGithub();
+
+      promise.then(function(response) {
+        let body = JSON.parse(response);
+        console.log(body);
+        let input = $("#condition").val();
+        body.data.forEach(function(index){
+          for (let i = 0; i < index.specialties.length; i++) {
+            if (index.specialties[i].description.match(input)) {
+              let pic = index.profile.image_url;
+              $(".output").append("<img src='" + pic + "'>");
+              let bio = index.profile.bio;
+              $(".output").append("<p>" + bio + "</p>");
+              break;
+            }
+            // if (!this.text) {
+            //   alert("There are no doctors that meet that criteria in Portland, OR.")
+            // }
+          }
+        });
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      })
+    }
+
+
+  });
+
+  $(".lookName").click(function() {
+    $(".output").html("");
     let freshApi = new apiCall();
     let promise = freshApi.runGithub();
 
     promise.then(function(response) {
       let body = JSON.parse(response);
       console.log(body);
-      let input = $("#condition").val();
+      let input = $("#doctor").val();
       body.data.forEach(function(index){
         for (let i = 0; i < index.specialties.length; i++) {
           if (index.specialties[i].description.match(input)) {

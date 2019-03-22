8205ec2153647d32c377ba2c9a604bf555525c5d
Student <example@example.com>
2019-03-22 10:00:40 -0700

    add fail trigger to second input field if left empty.

--- src/main.js
+++ src/main.js
@@ -42,27 +42,30 @@ $(document).ready(function() {
 
   $(".lookName").click(function() {
     $(".output").html("");
-    let freshApi = new apiCall();
-    let promise = freshApi.runGithub();
+    if($("#doctor").val() == ""){
+      alert("Please input your doctor.");
+    } else {
+      let freshApi = new apiCall();
+      let promise = freshApi.runGithub();
 
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      console.log(body);
-      let input = $("#doctor").val();
-      body.data.forEach(function(index){
-        for (let i = 0; i < index.specialties.length; i++) {
-          if (index.specialties[i].description.match(input)) {
-            let pic = index.profile.image_url;
-            $(".output").append("<img src='" + pic + "'>");
-            let bio = index.profile.bio;
-            $(".output").append("<p>" + bio + "</p>");
-            break;
+      promise.then(function(response) {
+        let body = JSON.parse(response);
+        console.log(body);
+        let input = $("#doctor").val();
+        body.data.forEach(function(index){
+          for (let i = 0; i < index.specialties.length; i++) {
+            if (index.specialties[i].description.match(input)) {
+              let pic = index.profile.image_url;
+              $(".output").append("<img src='" + pic + "'>");
+              let bio = index.profile.bio;
+              $(".output").append("<p>" + bio + "</p>");
+              break;
+            }
           }
-        }
-      });
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    })
-
+        });
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      })
+    }
   });
 });
